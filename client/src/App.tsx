af682116317698515ada62bafada460f4c46b8ff
PannH <pannh2609@gmail.com>
2022-12-05 23:18:08 +0100

    :lipstick: Stylize the `App`

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,7 +4,10 @@ function App() {
 
    return (
       <div className='app'>
-         <h1>Hello, World!</h1>
+         <h3>Some pretty good long title !</h3>
+         <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum provident tenetur voluptate sit cumque beatae vitae incidunt facere earum sed, blanditiis veniam, dolores repellat iure nulla nesciunt voluptatum odit commodi!
+         </p>
       </div>
    )
 
