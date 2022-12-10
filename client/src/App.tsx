5991f5eea3c3a2b55c3e346d4697d0de6c4606b0
PannH <pannh2609@gmail.com>
2022-12-10 16:05:27 +0100

    :sparkles: Set up React Query

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import Header from './components/Header';
+import TodoContainer from './components/TodoContainer';
+
+const queryClient = new QueryClient();
 
 function App() {
 
    return (
-      <div className='main'>
-         <Header />
-      </div>
+      <QueryClientProvider client={ queryClient }>
+         <div className='main'>
+            <Header />
+            <TodoContainer />
+         </div>
+      </QueryClientProvider>
    )
 
 }
